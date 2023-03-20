a2939d1c05be72628c1c5366ed62b387563dc3b0
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-20 14:41:40 -0400

    add edit feature

--- express_server.js
+++ express_server.js
@@ -48,6 +48,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect('/urls');
+});
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
