e8bbe3fc3acbaaeb3f5cb066b02256306c0bb055
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-20 15:46:53 -0400

    add logout feature

--- express_server.js
+++ express_server.js
@@ -61,6 +61,11 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls');
